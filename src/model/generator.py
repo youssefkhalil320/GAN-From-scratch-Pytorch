728fb6cb2df53b9abb0b0a3ddf1fe366ad88d312
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-07-05 01:13:11 +0300

    Add generator module

---
+++ src/model/generator.py
@@ -0,0 +1,31 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# Generate Fake Data: output like real data [1, 28, 28] and values -1, 1
+class Generator(nn.Module):
+    def __init__(self, latent_dim):
+        super().__init__()
+        self.lin1 = nn.Linear(latent_dim, 7*7*64)  # [n, 256, 7, 7]
+        self.ct1 = nn.ConvTranspose2d(64, 32, 4, stride=2)  # [n, 64, 16, 16]
+        self.ct2 = nn.ConvTranspose2d(32, 16, 4, stride=2)  # [n, 16, 34, 34]
+        self.conv = nn.Conv2d(16, 1, kernel_size=7)  # [n, 1, 28, 28]
+
+    def forward(self, x):
+        # Pass latent space input into linear layer and reshape
+        x = self.lin1(x)
+        x = F.relu(x)
+        x = x.view(-1, 64, 7, 7)  # 256
+
+        # Upsample (transposed conv) 16x16 (64 feature maps)
+        x = self.ct1(x)
+        x = F.relu(x)
+
+        # Upsample to 34x34 (16 feature maps)
+        x = self.ct2(x)
+        x = F.relu(x)
+
+        # Convolution to 28x28 (1 feature map)
+        return self.conv(x)
