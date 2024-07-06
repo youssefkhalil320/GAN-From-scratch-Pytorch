9a33bdba1237fc5ba6f0dcb86578ff3fea56b178
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-07-07 00:26:49 +0300

    Add trainer code

--- train.py
+++ train.py
@@ -1,7 +1,15 @@
 from src.data_loader.MNISTDataModule import MNISTDataModule
 from src.models.GAN import GAN
+import torch
+import torchvision
+import pytorch_lightning as pl
+
+
+AVAIL_GPUS = min(1, torch.cuda.device_count())
 
 
 dm = MNISTDataModule()
 model = GAN()
-model.plot_imgs()
+
+trainer = pl.Trainer(max_epochs=20, gpus=AVAIL_GPUS)
+trainer.fit(model, dm)
