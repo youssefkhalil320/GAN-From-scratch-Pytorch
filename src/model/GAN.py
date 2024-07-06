c4d968b62172c15b262131b5aa9dc0242a5a21f0
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-07-07 00:09:42 +0300

    Finalize GAN module

--- src/model/GAN.py
+++ src/model/GAN.py
@@ -5,11 +5,73 @@ import torch
 import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 
 class GAN(pl.LightningModule):
     def __init__(self, latent_dim=100, lr=0.0002):
         super().__init__()
         self.save_hyperparameters()
-        self.generator = Generator(latent_dim=self.hpparams.latent_dim)
-        self.validation_z = torch.randn(6, self.hpparams.latent_dim)
+        self.generator = Generator(latent_dim=self.hparams.latent_dim)
+        self.validation_z = torch.randn(6, self.hparams.latent_dim)
+
+    def forward(self, z):
+        return self.generator(z)
+
+    def adversarial_loss(self, y_hat, y):
+        return F.binary_cross_entropy(y_hat, y)
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        real_imgs, _ = batch
+
+        z = torch.randn(real_imgs.shape[0], self.hparams.latent_dim)
+        z = z.type_as(real_imgs)
+
+        if optimizer_idx == 0:
+            fake_imgs = self(z)
+            y_hat = self.discriminator(fake_imgs)
+            y = torch.ones(real_imgs)
+            g_loss = self.adversarial_loss(y_hat, y)
+            log_dict = {'g_loss': g_loss}
+            return {"loss": g_loss, "progress_bar": log_dict, "log": log_dict}
+
+        if optimizer_idx == 1:
+            y_hat_real = self.discriminator(real_imgs)
+
+            y_real = torch.ones(real_imgs.size(0), 1)
+            y_real = y_real.type_as(real_imgs)
+
+            real_loss = self.adversarial_loss(y_hat_real, y_real)
+            y_hat_fake = self.discriminator(self(z).detach())
+            y_fake = torch.zeros(real_imgs.size(0), 1)
+            y_fake = y_fake.type_as(real_imgs)
+            fake_loss = self.adversarial_loss(y_hat_fake, y_fake)
+            d_loss = (real_loss + fake_loss) / 2
+            log_dict = {'g_loss': d_loss}
+            return {"loss": d_loss, "progress_bar": log_dict, "log": log_dict}
+
+    def configure_optimizers(self):
+        lr = self.hpparams.lr
+        opt_g = torch.optim.Adam(self.generator.parameters(), lr=lr)
+        opt_d = torch.optim.Adam(self.discriminator.parameters(), lr=lr)
+        return [opt_g, opt_d], []
+
+    def plot_imgs(self):
+        z = self.validation_z.type_as(self.generator.lin1.weight)
+        sample_imgs = self(z).cpu()
+        print('epoch: ', self.current_epoch)
+        fig = plt.figure()
+        for i in range(sample_imgs.size(0)):
+            plt.subplot(2, 3, i+1)
+            plt.tight_layout()
+            plt.imshow(sample_imgs.detach()[
+                       i, 0, :, :], cmap='gray_r', interpolation='none')
+            plt.title('Generated Data')
+            plt.xticks([])
+            plt.yticks([])
+            plt.axis('off')
+
+        plt.show()
+
+    def on_epoch_end(self):
+        self.plot_imgs()
